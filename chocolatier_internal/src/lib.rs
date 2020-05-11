ae0991211932a1343417ae2bec9b72fb363151dd
Vincent Isambart <vincent.isambart@gmail.com>
2020-05-11 12:55:04 +0900

    At first do not modify the Rust AST

--- chocolatier_internal/src/lib.rs
+++ chocolatier_internal/src/lib.rs
@@ -18,23 +18,17 @@ fn empty_item() -> syn::Item {
     syn::Item::Verbatim(TokenStream::new())
 }
 
-// TODO: It might be nice to put the 'extern "C"' block where the first import was.
-fn extract_imports(items: &mut [syn::Item]) -> syn::Result<Vec<(usize, custom_parse::Import)>> {
-    let mut imports = Vec::new();
-    for (idx, item) in items
-        .iter_mut()
+fn read_imports(items: &[syn::Item]) -> syn::Result<Vec<custom_parse::Import>> {
+    let imports = items
+        .iter()
         .enumerate()
-        .filter(|(_, item)| matches!(item, syn::Item::Macro(mac) if is_import_macro(mac)))
-    {
-        let mut extracted_item = empty_item();
-        std::mem::swap(item, &mut extracted_item);
-        let mac = match extracted_item {
-            syn::Item::Macro(mac) => mac,
-            _ => unreachable!(),
-        };
-        let import = syn::parse2::<custom_parse::Import>(mac.mac.tokens)?;
-        imports.push((idx, import))
-    }
+        .map(|(_, item)| item)
+        .filter_map(|item| match item {
+            syn::Item::Macro(mac) if is_import_macro(mac) => Some(mac),
+            _ => None,
+        })
+        .map(|mac| syn::parse2::<custom_parse::Import>(mac.mac.tokens.clone()))
+        .collect::<syn::Result<Vec<_>>>()?;
 
     Ok(imports)
 }
@@ -338,10 +332,9 @@ fn resolve_uses(
     Ok(())
 }
 
-fn parse_imported_objc<'a, I>(imports: I) -> syn::Result<objc_parser::index::TypeIndex>
-where
-    I: Iterator<Item = &'a custom_parse::Import>,
-{
+fn parse_imported_objc(
+    imports: &[custom_parse::Import],
+) -> syn::Result<objc_parser::index::TypeIndex> {
     use std::fmt::Write;
 
     let mut objc_code = String::new();
@@ -422,153 +415,144 @@ fn signed_or_not_to_lit(val: objc_parser::ast::SignedOrNotInt) -> proc_macro2::L
     }
 }
 
-struct RustProcessor {
-    objc_index: objc_parser::index::TypeIndex,
-}
+fn process_trait_item(
+    _objc_index: &objc_parser::index::TypeIndex,
+    struc: &mut syn::ItemTrait,
+) -> syn::Result<()> {
+    let (_, args) = match extract_chocolatier_attr(&mut struc.attrs)? {
+        Some((attr, args)) => (attr, args),
+        None => return Ok(()),
+    };
 
-impl RustProcessor {
-    fn process_trait_item(&self, struc: &mut syn::ItemTrait) -> syn::Result<()> {
-        let (_, args) = match extract_chocolatier_attr(&mut struc.attrs)? {
-            Some((attr, args)) => (attr, args),
-            None => return Ok(()),
-        };
-
-        match args {
-            custom_parse::ItemArgs::Interface(_) | custom_parse::ItemArgs::Protocol(_) => {}
-            custom_parse::ItemArgs::Enum(enu) => {
-                return Err(syn::Error::new_spanned(
-                    enu.enum_token,
-                    "traits should not need an enum declaration",
-                ));
-            }
-        }
-
-        Ok(())
-    }
-
-    fn process_struct_item(&self, struc: &mut syn::ItemStruct) -> syn::Result<()> {
-        let (attr, args) = match extract_chocolatier_attr(&mut struc.attrs)? {
-            Some((attr, args)) => (attr, args),
-            None => return Ok(()),
-        };
-
-        match args {
-            custom_parse::ItemArgs::Enum(_) | custom_parse::ItemArgs::Interface(_) => {}
-            custom_parse::ItemArgs::Protocol(protocol) => {
-                return Err(syn::Error::new_spanned(
-                    protocol.protocol_token,
-                    "protocols should not need a struct declaration",
-                ));
-            }
-        }
-
-        if !struc.attrs.iter().any(is_repr_transparent) {
+    match args {
+        custom_parse::ItemArgs::Interface(_) | custom_parse::ItemArgs::Protocol(_) => {}
+        custom_parse::ItemArgs::Enum(enu) => {
             return Err(syn::Error::new_spanned(
-                attr,
-                "chocolatier structs are expected to be #[repr(transparent)]",
+                enu.enum_token,
+                "traits should not need an enum declaration",
             ));
         }
-
-        // TODO: For enums, check that type of first field is the same as the one of the C type.
-
-        Ok(())
     }
 
-    fn get_objc_enum_defs(
-        &self,
-        objc_name: &Ident,
-    ) -> syn::Result<Vec<&objc_parser::ast::EnumDef>> {
-        use objc_parser::ast;
+    Ok(())
+}
 
-        let mut enum_defs = Vec::new();
-        let str_name = objc_name.to_string();
-        let tag_id = ast::TagId::Named(str_name.clone());
-        match self.objc_index.enums.get(&tag_id) {
-            Some(objc_def) => enum_defs.push(objc_def),
-            None => match self.objc_index.typedefs.get(&str_name) {
-                Some(_) => {}
-                None => {
-                    return Err(syn::Error::new_spanned(
-                        objc_name,
-                        std::format!("could not find enum type {}", str_name),
-                    ));
-                }
-            },
+fn process_struct_item(
+    objc_index: &objc_parser::index::TypeIndex,
+    struc: &mut syn::ItemStruct,
+) -> syn::Result<()> {
+    let (attr, args) = match extract_chocolatier_attr(&mut struc.attrs)? {
+        Some((attr, args)) => (attr, args),
+        None => return Ok(()),
+    };
+
+    match args {
+        custom_parse::ItemArgs::Enum(_) | custom_parse::ItemArgs::Interface(_) => {}
+        custom_parse::ItemArgs::Protocol(protocol) => {
+            return Err(syn::Error::new_spanned(
+                protocol.protocol_token,
+                "protocols should not need a struct declaration",
+            ));
         }
+    }
 
-        for enu in self.objc_index.enums.values() {
-            match &enu.underlying.ty {
-                ast::Type::Typedef(typedef) if typedef.name == str_name => {}
-                ast::Type::Tag(tag) if tag.id == tag_id => {}
-                _ => continue,
+    if !struc.attrs.iter().any(is_repr_transparent) {
+        return Err(syn::Error::new_spanned(
+            attr,
+            "chocolatier structs are expected to be #[repr(transparent)]",
+        ));
+    }
+
+    // TODO: For enums, check that type of first field is the same as the one of the C type.
+
+    Ok(())
+}
+
+fn get_objc_enum_defs<'objc>(
+    objc_index: &'objc objc_parser::index::TypeIndex,
+    objc_name: &Ident,
+) -> syn::Result<Vec<&'objc objc_parser::ast::EnumDef>> {
+    use objc_parser::ast;
+
+    let mut enum_defs = Vec::new();
+    let str_name = objc_name.to_string();
+    let tag_id = ast::TagId::Named(str_name.clone());
+    match objc_index.enums.get(&tag_id) {
+        Some(objc_def) => enum_defs.push(objc_def),
+        None => match objc_index.typedefs.get(&str_name) {
+            Some(_) => {}
+            None => {
+                return Err(syn::Error::new_spanned(
+                    objc_name,
+                    std::format!("could not find enum type {}", str_name),
+                ));
             }
-            enum_defs.push(enu);
-        }
-
-        Ok(enum_defs)
+        },
     }
 
-    fn process_enum_impl(
-        &self,
-        enum_args: &custom_parse::EnumArgs,
-        imp: &mut syn::ItemImpl,
-    ) -> syn::Result<()> {
-        use syn::spanned::Spanned;
-
-        let defs = self.get_objc_enum_defs(&enum_args.objc_name)?;
-        // For all "const CONST_NAME: Self = ObjCEnumValue;" with ObjCEnumValue being an ObjC enum value,
-        // replace it by "Self(xxxx)", with xxxx being the real numerical value in ObjC.
-        for item in imp.items.iter_mut() {
-            let cnst = match item {
-                syn::ImplItem::Const(cnst) => cnst,
-                _ => continue,
-            };
-            let expr_path = match &cnst.expr {
-                syn::Expr::Path(expr_path) => expr_path,
-                _ => continue,
-            };
-            if let Some(ident) = expr_path.path.get_ident() {
-                if let Some(val) = defs
-                    .iter()
-                    .filter_map(|def| def.values.iter().filter(|val| ident == &val.name).next())
-                    .next()
-                {
-                    let mut lit = signed_or_not_to_lit(val.value);
-                    lit.set_span(cnst.expr.span());
-                    cnst.expr = syn::parse_quote! {Self(#lit)};
-                }
-            }
+    for enu in objc_index.enums.values() {
+        match &enu.underlying.ty {
+            ast::Type::Typedef(typedef) if typedef.name == str_name => {}
+            ast::Type::Tag(tag) if tag.id == tag_id => {}
+            _ => continue,
         }
-
-        Ok(())
+        enum_defs.push(enu);
     }
 
-    fn process_impl_item(&self, imp: &mut syn::ItemImpl) -> syn::Result<()> {
-        // Only interested in impl blocks marked "#[chocolatier(xxxx = xxxxx)]".
-        let (_, args) = match extract_chocolatier_attr(&mut imp.attrs)? {
-            Some((attr, args)) => (attr, args),
-            None => return Ok(()),
+    Ok(enum_defs)
+}
+
+fn process_enum_impl(
+    objc_index: &objc_parser::index::TypeIndex,
+    enum_args: &custom_parse::EnumArgs,
+    imp: &mut syn::ItemImpl,
+) -> syn::Result<()> {
+    use syn::spanned::Spanned;
+
+    let defs = get_objc_enum_defs(&objc_index, &enum_args.objc_name)?;
+    // For all "const CONST_NAME: Self = ObjCEnumValue;" with ObjCEnumValue being an ObjC enum value,
+    // replace it by "Self(xxxx)", with xxxx being the real numerical value in ObjC.
+    for item in imp.items.iter_mut() {
+        let cnst = match item {
+            syn::ImplItem::Const(cnst) => cnst,
+            _ => continue,
         };
-
-        match args {
-            custom_parse::ItemArgs::Enum(enum_args) => self.process_enum_impl(&enum_args, imp)?,
-            _ => {}
-        }
-
-        Ok(())
-    }
-
-    fn process(&self, items: &mut Vec<syn::Item>) -> syn::Result<()> {
-        for item in items.iter_mut() {
-            match item {
-                syn::Item::Struct(struc) => self.process_struct_item(struc)?,
-                syn::Item::Impl(imp) => self.process_impl_item(imp)?,
-                syn::Item::Trait(trai) => self.process_trait_item(trai)?,
-                _ => {}
+        let expr_path = match &cnst.expr {
+            syn::Expr::Path(expr_path) => expr_path,
+            _ => continue,
+        };
+        if let Some(ident) = expr_path.path.get_ident() {
+            if let Some(val) = defs
+                .iter()
+                .filter_map(|def| def.values.iter().filter(|val| ident == &val.name).next())
+                .next()
+            {
+                let mut lit = signed_or_not_to_lit(val.value);
+                lit.set_span(cnst.expr.span());
+                cnst.expr = syn::parse_quote! {Self(#lit)};
             }
         }
-        Ok(())
     }
+
+    Ok(())
+}
+
+fn process_impl_item(
+    objc_index: &objc_parser::index::TypeIndex,
+    imp: &mut syn::ItemImpl,
+) -> syn::Result<()> {
+    // Only interested in impl blocks marked "#[chocolatier(xxxx = xxxxx)]".
+    let (_, args) = match extract_chocolatier_attr(&mut imp.attrs)? {
+        Some((attr, args)) => (attr, args),
+        None => return Ok(()),
+    };
+
+    match args {
+        custom_parse::ItemArgs::Enum(enum_args) => process_enum_impl(objc_index, &enum_args, imp)?,
+        _ => {}
+    }
+
+    Ok(())
 }
 
 pub fn chocolatier(input: TokenStream) -> syn::Result<TokenStream> {
@@ -583,7 +567,7 @@ pub fn chocolatier(input: TokenStream) -> syn::Result<TokenStream> {
         ));
     };
 
-    let imports = extract_imports(items)?;
+    let imports = read_imports(items)?;
 
     if imports.is_empty() {
         return Err(syn::Error::new(
@@ -592,15 +576,20 @@ pub fn chocolatier(input: TokenStream) -> syn::Result<TokenStream> {
         ));
     }
 
-    let objc_index = parse_imported_objc(imports.iter().map(|(_, import)| import))?;
+    let objc_index = parse_imported_objc(&imports)?;
 
     let uses = find_objc_macro_uses(items)?;
 
     resolve_uses(&objc_index, &uses)?;
 
-    let processor = RustProcessor { objc_index };
-
-    processor.process(items)?;
+    for item in items.iter_mut() {
+        match item {
+            syn::Item::Struct(struc) => process_struct_item(&objc_index, struc)?,
+            syn::Item::Impl(imp) => process_impl_item(&objc_index, imp)?,
+            syn::Item::Trait(trai) => process_trait_item(&objc_index, trai)?,
+            _ => {}
+        }
+    }
 
     Ok(item.into_token_stream())
 }
